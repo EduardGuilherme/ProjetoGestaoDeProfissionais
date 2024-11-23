25e9a1565f4f2bb10fd9c087c5977246cb18a6eb
EduardGuilherme <eduguilherme48@gmail.com>
2024-11-23 01:55:28 -0300

    add Map

---
+++ gestaoDeProfissionais/Data/Map/EspecialidadeMap.cs
@@ -0,0 +1,21 @@
+﻿using gestaoDeProfissionais.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace gestaoDeProfissionais.Data.Map
+{
+    public class EspecialidadeMap : IEntityTypeConfiguration<Especialidade>
+    {
+        public void Configure(EntityTypeBuilder<Especialidade> builder)
+        {
+            builder.ToTable("Especialidades");
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(e => e.TipoDocumento)
+                .IsRequired()
+                .HasMaxLength(10);
+        }
+    }
+}
---
+++ gestaoDeProfissionais/Data/Map/ProfissinalMap.cs
@@ -0,0 +1,25 @@
+﻿using gestaoDeProfissionais.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace gestaoDeProfissionais.Data.Map
+{
+    public class ProfissinalMap : IEntityTypeConfiguration<Profissional>
+    {
+        public void Configure(EntityTypeBuilder<Profissional> builder)
+        {
+            builder.ToTable("Profissionais");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(p => p.NumeroDocumento)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.HasOne(p => p.Especialidade)
+                .WithMany() 
+                .HasForeignKey(p => p.EspecialidadeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
