9d5971eece399673dcf7d8123a59176cefda9c1e
EduardGuilherme <eduguilherme48@gmail.com>
2024-11-23 21:38:46 -0300

    add repository and interface

---
+++ gestaoDeProfissionais/Repository/Interface/IProfissional.cs
@@ -0,0 +1,16 @@
+﻿using gestaoDeProfissionais.Models;
+using System.Linq.Expressions;
+
+namespace gestaoDeProfissionais.Repository.Interface
+{
+    public interface IProfissional
+    {
+        Task<IEnumerable<Profissional>> GetAllAsync();
+        Task<Profissional?> GetByIdAsync(int id);
+        Task<Profissional> AddAsync(Profissional profissional);
+        Task<Profissional> UpdateAsync(Profissional profissional);
+        Task DeleteAsync(int id);
+        Task SaveChangesAsync();
+
+    }
+}
---
+++ gestaoDeProfissionais/Repository/ProfissionalRepository.cs
@@ -0,0 +1,50 @@
+﻿using gestaoDeProfissionais.Data;
+using gestaoDeProfissionais.Models;
+using gestaoDeProfissionais.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace gestaoDeProfissionais.Repository
+{
+    public class ProfissionalRepository : IProfissional
+    {
+        private readonly GestaoDeProfissionaisDBContext context;
+
+        public ProfissionalRepository(GestaoDeProfissionaisDBContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Profissional>> GetAllAsync()
+        {
+            return await context.Profissionais.Include(p => p.Especialidade).ToListAsync();
+        }
+
+        public async Task<Profissional?> GetByIdAsync(int id)
+        {
+            return await context.Profissionais.Include(p => p.Especialidade).FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task<Profissional> AddAsync(Profissional profissional)
+        {
+            var result = await context.Profissionais.AddAsync(profissional);
+            return result.Entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var profissional = await context.Profissionais.FindAsync(id);
+            if (profissional != null)
+            {
+                context.Profissionais.Remove(profissional);
+            }
+        }
+        public Task<Profissional> UpdateAsync(Profissional profissional)
+        {
+            context.Profissionais.Update(profissional);
+            return Task.FromResult(profissional);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
